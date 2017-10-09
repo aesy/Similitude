bdd7af43a2528f4ba090bfbb4245a9e6e514201d
Isak W <isak.wertwein@gmail.com>
2017-10-09 18:16:09 +0200

    Documentation

--- Similitude/Core/ISimilarityResolver.cs
+++ Similitude/Core/ISimilarityResolver.cs
@@ -1,6 +1,3 @@
-using System;
-using System.Collections;
-using System.Collections.Generic;
 using JetBrains.Annotations;
 
 namespace Similitude.Core
@@ -8,6 +5,13 @@ namespace Similitude.Core
     [PublicAPI]
     public interface ISimilarityResolver<in T>
     {
+        /// <summary>
+        /// Compares two elements of arbitrary type <typeparamref name="T"/> and resolves a normalized result
+        /// indicating their similarity.
+        /// </summary>
+        /// <param name="first">The first element.</param>
+        /// <param name="second">The second element.</param>
+        /// <returns>A number in the range [0, 1].</returns>
         double GetSimilarity(T first, T second);
     }
 }
